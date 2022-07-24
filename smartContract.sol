f35d98438608d2661da976d0cb4f489c860f688c
iceBlender <76730792+xalsius@users.noreply.github.com>
2022-07-24 17:52:55 +0300

    zinder smart contract

---
+++ smartContract.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Zinder {
+    uint256 public counter;
+    mapping(address => uint256) public userId;
+    mapping(uint256 => string) public userStreamId;
+
+    mapping(address => address[]) public merkleRoot;
+
+    constructor() {
+        counter = 0;
+    }
+
+    function registerUser(address merkle, string streamId) public {
+        if (userId[msg.sender] == 0) {
+            counter += 1;
+            userId[msg.sender] = counter;
+
+            updateMerkle(merkle);
+            userStreamId[counter] = streamId;
+        }
+    }
+
+    function updateMerkle(address merkle) public {
+        merkleRoot[msg.sender].push(merkle);
+    }
+
+    function rootBelongsToUser(address userEOA, address merkle)
+        public
+        returns (boolean)
+    {
+        uint i;
+        uint len = merkleRoot[userEOA].length;
+        for (i = 0; i < len; i++) {
+            if (merkleRoot[userEOA][i] == merkle) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
